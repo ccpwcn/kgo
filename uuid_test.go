9e4bcc44307040f63034f95857ef15e2d46a219f
ccpwcn <ccpwcn@gmail.com>
2023-03-18 15:39:06 +0800

    降低压力测试数据量，避免测试执行时间过长被github取消

--- uuid_test.go
+++ uuid_test.go
@@ -33,7 +33,7 @@ func Test_SimpleUuid(t *testing.T) {
 
 func Test_Uuid_Million(t *testing.T) {
 	m := make(map[string]bool)
-	for i := 1; i < 100_0000; i++ {
+	for i := 1; i < 2_000_000; i++ {
 		id := Uuid()
 		if m[id] {
 			t.Errorf("duplicated UUID %s", id)
@@ -44,29 +44,7 @@ func Test_Uuid_Million(t *testing.T) {
 
 func Test_SimpleUuid_Million(t *testing.T) {
 	m := make(map[string]bool)
-	for i := 1; i < 100_0000; i++ {
-		id := SimpleUuid()
-		if m[id] {
-			t.Errorf("duplicated UUID %s", id)
-		}
-		m[id] = true
-	}
-}
-
-func Test_Uuid_Billion(t *testing.T) {
-	m := make(map[string]bool)
-	for i := 1; i < 1_0000_0000; i++ {
-		id := Uuid()
-		if m[id] {
-			t.Errorf("duplicated UUID %s", id)
-		}
-		m[id] = true
-	}
-}
-
-func Test_SimpleUuid_Billion(t *testing.T) {
-	m := make(map[string]bool)
-	for i := 1; i < 1_0000_0000; i++ {
+	for i := 1; i < 2_000_000; i++ {
 		id := SimpleUuid()
 		if m[id] {
 			t.Errorf("duplicated UUID %s", id)
@@ -79,7 +57,7 @@ func Benchmark_Uuid(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		var buffer sync.Map
-		for i := 0; i < 500_0000; i++ {
+		for i := 0; i < 5_000_000; i++ {
 			b.RunParallel(func(pb *testing.PB) {
 				for pb.Next() {
 					id := Uuid()
@@ -97,7 +75,7 @@ func Benchmark_SimpleUuid(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		var buffer sync.Map
-		for i := 0; i < 500_0000; i++ {
+		for i := 0; i < 5_000_000; i++ {
 			b.RunParallel(func(pb *testing.PB) {
 				for pb.Next() {
 					id := SimpleUuid()
