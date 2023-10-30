238d6ddebba2abe18490ee4ee6e33f83e308b20e
ccpwcn <ccpwcn@gmail.com>
2023-10-30 19:11:44 +0800

    增加字符串和byte切片转换功能

--- str.go
+++ str.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"unsafe"
 )
 
 // 英文特殊符号
@@ -49,4 +50,19 @@ func JoinElements[T IntUintFloat | IntUintFloatPtr | string | *string | bool | *
 		}
 		return strings.TrimRight(s, ",")
 	}
+}
+
+// B2S byte切片转为string
+func B2S(b []byte) (s string) {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// S2B string转为byte切片
+func S2B(s string) (b []byte) {
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Cap = sh.Len
+	bh.Len = sh.Len
+	return b
 }
