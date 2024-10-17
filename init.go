ba01794152b2f05b15e535257b568ffc427a166b
ccpwcn <ccpwcn@gmail.com>
2024-10-17 16:39:34 +0800

    pref: 泛型中的int系、uint系支持优化

--- init.go
+++ init.go
@@ -1,7 +1,7 @@
 package kgo
 
 type IntUintBig interface {
-	~int | ~int64 | ~uint | ~uint64
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 type IntUintFloat interface {
